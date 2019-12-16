72551d4fa18efc7bbbc1712cd12d4d0d0f99e228
zamFe <zamfel18@student.kristiania.no>
2019-12-16 12:01:00 +0100

    added dynamic line to routedetails

--- Source/JS/stopgetter.js
+++ Source/JS/stopgetter.js
@@ -1,5 +1,6 @@
-let container = document.getElementById("main-container");
+var count = 0;
 
+let container = document.getElementById("main-container");
 
 //build step and adds to HTML
 function stepBuilder(action, stepName, stepTime) {
@@ -17,6 +18,12 @@ function stepBuilder(action, stepName, stepTime) {
     //build node element
     let stopNode = document.createElement("div");
     stopNode.id = "node-container";
+
+    if(count === 0) {
+        let routeLine = document.createElement("div");
+        routeLine.id = "route-line";
+        stopNode.appendChild(routeLine);
+    }
 
     let nodeImg = document.createElement("img");
     nodeImg.classList.add("Node");
@@ -44,6 +51,10 @@ function stepBuilder(action, stepName, stepTime) {
     step.appendChild(name);
 
     container.appendChild(step);
+    count++;
+
+    let line = document.getElementById("route-line");
+    line.style.height = (300 * (count - 1)) + "px";
 }
 
 function transitionBuilder(startTime, stopTime) {
@@ -72,6 +83,10 @@ function transitionBuilder(startTime, stopTime) {
     step.appendChild(name);
 
     container.appendChild(step);
+    count++;
+
+    let line = document.getElementById("route-line");
+    line.style.height = 300 * count + "px";
 }
 
 function convertTime(time) {
