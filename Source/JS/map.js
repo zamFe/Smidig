7a040758202bef093e38d93023a3b78c6a79577e
Andreas Østby <andreas88-ostby@hotmail.com>
2019-12-18 11:58:08 +0100

    Updated the map to zoom and pan according to the route data

--- Source/JS/map.js
+++ Source/JS/map.js
@@ -1,33 +1,45 @@
 var fullRoute = JSON.parse(localStorage.getItem("route"));
+let index = new URLSearchParams(window.location.search).get("index");
 var map;
 
+let coordFrom = {
+    lat: fullRoute[index].route[0].from.lat,
+    lng: fullRoute[index].route[0].from.lng
+}
+
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 63.1143, lng: 7.7343 },
-        zoom: 8
+        center: coordFrom,
+        zoom: 12
     });
 
-    let index = new URLSearchParams(window.location.search).get("index");
+    let bounds = new google.maps.LatLngBounds();
 
     for (var i = 0; i < fullRoute[index].route.length; i++) {
-        //console.log(fullRoute[index].route[i])
 
         for (var j = 0; j < fullRoute[index].route[i].waypoints.shapes.length; j++) {
 
-
-            console.log(fullRoute[index].route[i].waypoints.color.red, fullRoute[index].route[i].waypoints.color.green, fullRoute[index].route[i].waypoints.color.blue)
-            //console.log('#' + fullRoute[index].route[i].waypoints.color.red.toString(16) + fullRoute[index].route[i].waypoints.color.green.toString(16) + fullRoute[index].route[i].waypoints.color.blue.toString(16))
+            let shapes = google.maps.geometry.encoding.decodePath(fullRoute[index].route[i].waypoints.shapes[j].encodedWaypoints)
 
             var cascadiaFault = new google.maps.Polyline({
                 strokeColor: '#' + fullRoute[index].route[i].waypoints.color.red.toString(16) + fullRoute[index].route[i].waypoints.color.green.toString(16) + fullRoute[index].route[i].waypoints.color.blue.toString(16),
                 strokeOpacity: 1,
-                strokeWeight: 100,
-                path: google.maps.geometry.encoding.decodePath(fullRoute[index].route[i].waypoints.shapes[j].encodedWaypoints)
+                strokeWeight: 10,
+                path: shapes
             });
+
+            for (var a = 0; a < shapes.length; a++) {
+              bounds.extend(new google.maps.LatLng(shapes[a].lat(), shapes[a].lng()));     
+
+            }
 
             cascadiaFault.setMap(map);
         }
     }
+
+    map.fitBounds(bounds);
+    map.panToBounds(bounds);  
 
 }
 
