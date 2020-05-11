4d70fc310584a7181b9c8b3b4822122adc60c4d5
Krister <SeventhDisaster@outlook.com>
2020-05-11 14:42:17 +0200

    Sample user created

---
+++ src/server/db/user-repo.js
@@ -0,0 +1,16 @@
+let sampleUser = {
+    email: "ole-nordmann@yahoo.no",
+    firstName: "Ole",
+    lastName: "Nordmann",
+    password: "løfreaglrkehtgaerfhgajkdfhgkjadfghdf",
+    searchHistory: [
+        // A location object
+    ],
+    favorites: [
+        // Location object
+    ]
+}
+
+function createUser() {
+
+}
