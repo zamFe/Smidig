e11c1796fb3d98cd8f063a1ada9d4e0e6cd28ed8
Andreas Østby <andreas88-ostby@hotmail.com>
2020-05-13 12:41:28 +0200

    Added sha256 hash function Hashing on client side aswell as server

--- public/src/database.js
+++ public/src/database.js
@@ -1,31 +1,42 @@
 async function getUser(email, password) { // Login
-    const url = `${window.location.origin}/api/db/loginuser?email=${email}&password=${password}`
+    const url = `${window.location.origin}/api/db/loginuser?email=${email}&password=${hash(password)}`
 
     var response = await getDbRequest(url);
 
-    if (response.payload.statusCode === 200)
-        localStorage.setItem("user", JSON.stringify(response.payload.data));
+    if (response.payload.statusCode === 200) {
+        var modified = response.payload.data;
+        modified.password = password;
+        localStorage.setItem("user", JSON.stringify(modified));
+    }
 
     return response.payload;
 }
 
 async function updateUser(email, password, packet) {
-    const url = `${window.location.origin}/api/db/updateuser?email=${email}&password=${password}`
+    const url = `${window.location.origin}/api/db/updateuser?email=${email}&password=${hash(password)}`
 
     var response = await getDbRequest(url, JSON.stringify(packet));
 
-    if (response.payload.statusCode === 200)
-        localStorage.setItem("user", JSON.stringify(response.payload.data));
+    if (response.payload.statusCode === 200) {
+        var modified = response.payload.data;
+        modified.password = password;
+        localStorage.setItem("user", JSON.stringify(modified));
+    }
+
 
     return response.payload;
 }
 
 async function createUser(email, password, firstname, lastname) {
-    const url = `${window.location.origin}/api/db/createuser?email=${email}&password=${password}&firstname=${firstname}&lastname=${lastname}`
+    const url = `${window.location.origin}/api/db/createuser?email=${email}&password=${hash(password)}&firstname=${firstname}&lastname=${lastname}`
 
+    console.log(url)
     var response = await getDbRequest(url);
-    if (response.payload.statusCode === 200)
-        localStorage.setItem("user", JSON.stringify(response.payload.data));
+    if (response.payload.statusCode === 200) {
+        var modified = response.payload.data;
+        modified.password = password;
+        localStorage.setItem("user", JSON.stringify(modified));
+    }
 
     return response.payload;
 }
@@ -48,4 +59,8 @@ async function getDbRequest(url, body = null) {
         console.log(e);
     }
     return {response,payload};
+}
+
+function hash(password) {
+    return sha256(password + "ohohStinky")
 }
