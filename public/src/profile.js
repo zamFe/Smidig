5156d3b4608702679d5355f0a96b43d9fc2aa869
Andreas Sæther <andreassaether94@gmail.com>
2020-05-13 14:41:37 +0200

    Profile page now displays logged in user info

--- public/src/profile.js
+++ public/src/profile.js
@@ -1,7 +1,16 @@
 
-createUser("esmlsl", "passss", "andreas", "østny").then((value) => {
-    getUser("esmlsl", "passss").then((v) => {
-        console.log(v.data) 
+let firstNameSpan = document.getElementById("firstname-span");
+let lastNameSpan = document.getElementById("lastname-span");
+let emailSpan = document.getElementById("email-span");
+
+
+createUser("randomannet", "passss", "andreas", "østny").then((value) => {
+    getUser("randomannet", "passss").then((v) => {
+        console.log(v.data)
+
+        firstNameSpan.innerHTML = v.data.firstName;
+        lastNameSpan.innerHTML = v.data.lastName;
+        emailSpan.innerHTML = v.data.email;
+
     })
 })
-
