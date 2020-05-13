a01e81918a53bcfa580845ef44e2eb3ab3c487b1
zamFe <zamfel18@student.kristiania.no>
2020-05-13 14:49:40 +0200

    working on login.register code

--- public/src/login-register.js
+++ public/src/login-register.js
@@ -1,5 +1,19 @@
 
 
-function login() {}
+function login()
+{
+}
 
-function register() {}
+function register()
+{
+    let name = document.getElementById("name-input");
+    let surname = document.getElementById("surname-input");
+    let email = document.getElementById("email-input");
+    let password = document.getElementById("password-input");
+
+    createUser(email, password, name, surname).then((payLoad) => {
+        if(payLoad.statusCode != 200) {
+
+        }
+    })
+}
