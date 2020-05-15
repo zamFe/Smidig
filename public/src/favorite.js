915147704eb5a67ba623197d47313ed7dada8d81
tommyhama95 <42606938+tommyhama95@users.noreply.github.com>
2020-05-15 12:27:19 +0200

    Favorite can now add and delete a route

--- public/src/favorite.js
+++ public/src/favorite.js
@@ -1,12 +1,14 @@
 const title = document.getElementById("favourite-title");
 
-function loadFavoriteRoutes () {
+function loadFavoriteRoutes() {
     const list = document.getElementById("favourite-routes");
     const user = JSON.parse(localStorage.getItem("user"));
 
     if(user) {
-        const routes = user.favorites;
-        for(let item of routes) {
+
+        for(let item of user.favorites) {
+            const url = item.url;
+
             let listElement = document.createElement("div");
             listElement.className = "detail-box";
 
@@ -37,26 +39,48 @@ function loadFavoriteRoutes () {
             // Destination div container
             let routeDetail = document.createElement("div");
             routeDetail.className = "route-detail";
+            routeDetail.setAttribute("alt", url);
 
 
             // Destinations
             let listTitle = document.createElement("p");
             listTitle.addEventListener("click", () => {
-                alert("Clicked on destination")
+                window.location = routeDetail.getAttribute("alt");
             });
 
-            listTitle.innerText = `Fra: ${item.from} \n\n Til: ${item.to}`;
+            listTitle.innerHTML = `Fra: <b>${item.from}</b> <br><br> Til: <b>${item.to}</b>`;
             routeDetail.appendChild(listTitle);
-            routeDetail.setAttribute("alt", item.url);
 
             listElement.appendChild(routeDetail);
 
             // Delete button
-            const delImage = document.createElement("img");
+            const delImage = document.createElement("div");
             delImage.className = "delete-icon";
-            delImage.src = "res/img/icons/redx.png";
+            delImage.innerHTML =
+                "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 463.47 464.2\">\n" +
+                "    <g id=\"Layer_1\" data-name=\"Layer 1\">\n" +
+                "        <path class=\"cls-1\" d=\"M124.71,430.84l24.06-140.26L46.87,191.26l140.82-20.47,63-127.6,63,127.6,140.82,20.47-77.89,75.93A114.28,114.28,0,0,0,232,374.46Z\" transform=\"translate(-18.93 -13.82)\"/>\n" +
+                "    </g>\n" +
+                "    <g id=\"Layer_2\" data-name=\"Layer 2\">\n" +
+                "        <path class=\"cls-2\" d=\"M434.14,385a80,80,0,1,1-80-80A80,80,0,0,1,434.14,385ZM309,385H399.3\" transform=\"translate(-18.93 -13.82)\"/>\n" +
+                "    </g>\n" +
+                "</svg>";
             delImage.addEventListener("click", () => {
-                alert("You wanna remove me?");
+                user.favorites.forEach(route => {
+                    if(route.url === url) {
+                        user.favorites.splice(user.favorites.indexOf(route), 1);
+
+                        while(list.firstChild) { // Removes each Route element
+                            list.removeChild(list.firstChild);
+                        }
+                         updateUser(user.email, user.password, {favorites: user.favorites}).then(
+                         // Needs to wait on the promise from API that data was updated
+                         // on user before loading new layout. Otherwise layout
+                         // keeps deleted route
+                             () => { loadFavoriteRoutes(); }
+                         );
+                    }
+                });
             });
 
 
@@ -69,7 +93,7 @@ function loadFavoriteRoutes () {
     } else {
         title.innerText = "Du har ingen i favoritter";
     }
-
+    console.log(user)
 }
 
 loadFavoriteRoutes();
