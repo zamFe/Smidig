849091112b56d0830c3a6ce47f66d0c0d073b880
tommyhama95 <42606938+tommyhama95@users.noreply.github.com>
2020-05-15 13:10:20 +0200

    Login styling altered, logo follows company's requirement

--- public/src/login-register.js
+++ public/src/login-register.js
@@ -1,9 +1,8 @@
-function login()
-{
+function login() {
     let email = document.getElementById("username-input").value;
-
     let password = document.getElementById("password-input").value;
 
+    // Wait for feedback on login
     getUser(email, password).then((payLoad) => {
         if(payLoad.statusCode == 200) {
            window.location.href = "./profile.html";
@@ -11,21 +10,21 @@ function login()
         else {
             alert(payLoad.status);
         }
-    })
+    });
 }
 
-function register()
-{
+function register() {
     let name = document.getElementById("name-input").value;
     let surname = document.getElementById("surname-input").value;
     let email = document.getElementById("email-input").value;
     let password = document.getElementById("password-input").value;
 
+    // Wait for feedback of registration
     createUser(email, password, name, surname).then((payLoad) => {
         if (payLoad.statusCode != 200) {
             alert(payLoad.status);
         } else {
             window.location.href = "./profile.html";
         }
-    })
+    });
 }
