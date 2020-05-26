df18fb18936b33a6a19aa74010c4aac4d67d4e14
Krister <SeventhDisaster@outlook.com>
2020-05-26 14:45:59 +0200

    Updated styling of Favorites page, now matches more with styles of other pages

--- public/src/favorite.js
+++ public/src/favorite.js
@@ -1,102 +1,97 @@
+function searchRoute(url) {
+    //window.location.href = url //Redirect to the search
+}
+
+function removeFavorite(url) {
+    event.stopPropagation();
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    console.log(user)
+
+    user.favorites.forEach(route => {
+        if (route.url === url) {
+            user.favorites.splice(user.favorites.indexOf(route), 1);
+
+            updateUser(user.email, user.password, {favorites: user.favorites}).then(
+                // Needs to wait on the promise from API that data was updated
+                // on user before loading new layout. Otherwise layout
+                // keeps deleted route
+                () => {
+                    loadFavoriteRoutes();
+                }
+            );
+        }
+    });
+}
+
 function loadFavoriteRoutes() {
-    const title = document.getElementById("favorite-title");
+    const info = document.getElementById("info-field")
     const list = document.getElementById("favorite-routes");
     const user = JSON.parse(localStorage.getItem("user"));
 
+    list.innerHTML = "";
+
     if(user) {
+        console.log(user.favorites)
         if (user.favorites.length > 0) {
             for (let item of user.favorites) {
                 const url = item.url;
+                const from = item.from;
+                const fromName = from.indexOf(',') < 1 ? from : from.substring(0,from.indexOf(','))
+                const fromArea = from.indexOf(',') < 1 ? "" : from.substring(from.indexOf(',') + 1)
 
-                let listElement = document.createElement("div");
-                listElement.className = "detail-box";
+                const to = item.to;
+                const toName = to.indexOf(',') < 1 ? to : to.substring(0, to.indexOf(','))
+                const toArea = to.indexOf(',') < 1 ? "" : to.substring(to.indexOf(',') + 1)
 
-                // Route line div
-                let routeLine = document.createElement("div");
-                routeLine.className = "route-line";
+                let template = `
+                    <div class="item" onclick="searchRoute('${url}')">
+                        <div class="vs-container">
+                            <div class="vs-ball"></div>
+                            <div class="vs-line"></div>
+                            <div class="vs-ball"></div>
+                        </div>
+                        <div class="route">
+                            <div class="from-container">
+                                <p class="name">${fromName}</p>
+                                <p class="area">${fromArea}</p>
+                            </div>                            
+                            <div class="to-container">
+                                <p class="name">${toName}</p>
+                                <p class="area">${toArea}</p>
+                            </div>
+                        </div>
+                        <div class="remove" onclick="removeFavorite('${url}')">
+                            <svg class="rm-fav-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 463.47 464.2"><g class="fav" data-name="Favorite Star"><path class="star-icon" d="M124.71,430.84l24.06-140.26L46.87,191.26l140.82-20.47,63-127.6,63,127.6,140.82,20.47-77.89,75.93A114.28,114.28,0,0,0,232,374.46Z" transform="translate(-18.93 -13.82)"/></g><g class="rm-fav" data-name="Remove Favorite"><path class="minus-icon" d="M434.14,385a80,80,0,1,1-80-80A80,80,0,0,1,434.14,385ZM309,385H399.3" transform="translate(-18.93 -13.82)"/></g></svg>
+                        </div>
+                    </div>
+                `
 
-                // Node creation 1 and 2
-                let listNode1 = document.createElement("img");
-                listNode1.className = "node-icon";
-                listNode1.src = "../../res/img/icons/node.png";
-
-                let listNode2 = document.createElement("img");
-                listNode2.className = "node-icon";
-                listNode2.src = "../../res/img/icons/node.png";
-
-                // Line in the node
-                let listLine = document.createElement("div");
-                listLine.className = "line";
-
-                // Appending destination line travel display
-                routeLine.appendChild(listNode1)
-                routeLine.appendChild(listLine)
-                routeLine.appendChild(listNode2);
-                listElement.appendChild(routeLine);
-
-
-                // Destination div container
-                let routeDetail = document.createElement("div");
-                routeDetail.className = "route-detail";
-                routeDetail.setAttribute("alt", url);
-
-
-                // Destinations
-                let listTitle = document.createElement("p");
-                listTitle.addEventListener("click", () => {
-                    window.location = routeDetail.getAttribute("alt");
-                });
-
-                listTitle.innerHTML = `Fra: <b>${item.from}</b> <br><br> Til: <b>${item.to}</b>`;
-                routeDetail.appendChild(listTitle);
-
-                listElement.appendChild(routeDetail);
-
-                // Delete button
-                const delImage = document.createElement("div");
-                delImage.className = "delete-icon";
-                delImage.innerHTML =
-                    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 463.47 464.2\">\n" +
-                    "    <g id=\"Layer_1\" data-name=\"Layer 1\">\n" +
-                    "        <path class=\"cls-1\" d=\"M124.71,430.84l24.06-140.26L46.87,191.26l140.82-20.47,63-127.6,63,127.6,140.82,20.47-77.89,75.93A114.28,114.28,0,0,0,232,374.46Z\" transform=\"translate(-18.93 -13.82)\"/>\n" +
-                    "    </g>\n" +
-                    "    <g id=\"Layer_2\" data-name=\"Layer 2\">\n" +
-                    "        <path class=\"cls-2\" d=\"M434.14,385a80,80,0,1,1-80-80A80,80,0,0,1,434.14,385ZM309,385H399.3\" transform=\"translate(-18.93 -13.82)\"/>\n" +
-                    "    </g>\n" +
-                    "</svg>";
-                delImage.addEventListener("click", () => {
-                    user.favorites.forEach(route => {
-                        if (route.url === url) {
-                            user.favorites.splice(user.favorites.indexOf(route), 1);
-
-                            while (list.firstChild) { // Removes each Route element
-                                list.removeChild(list.firstChild);
-                            }
-                            updateUser(user.email, user.password, {favorites: user.favorites}).then(
-                                // Needs to wait on the promise from API that data was updated
-                                // on user before loading new layout. Otherwise layout
-                                // keeps deleted route
-                                () => {
-                                    loadFavoriteRoutes();
-                                }
-                            );
-                        }
-                    });
-                });
-
-                listElement.appendChild(delImage);
-
-                // Appending to parent div
-                list.appendChild(listElement)
+                list.innerHTML += template;
             }
         } else {
-            // No route in users favorite list
-            list.innerText = "Du har ingen reiser lagt til";
-            list.className = "text-empty-route";
+            const message = "Du har ingen reiser markert som favoritt"
+            const infoMessage = "Legg til favoritter ved å trykke på stjerneikonet under rutesøk"
+
+            const template = `
+                <div class="msg-cont">
+                    <p class="no-faves">${message}</p>
+                </div>
+            `
+            info.innerText = infoMessage;
+            list.innerHTML = template;
         }
     } else {
-        // User not logged in
-        title.innerText = "Du må logge inn";
+        const message = "Du er ikke logget inn"
+
+        info.innerText = "";
+        const template = `
+            <div class="msg-cont">
+                <p class="no-faves">${message}</p>
+            </div>
+            `
+
+        list.innerHTML = template;
     }
 }
 
